99d4a324c6d94a3a3c6c61686b1dc9d8a27c7385
Ivy Ivanova <ivaivanova091@gmail.com>
2020-05-25 12:32:09 +0100

    [QTMessageBox] message that comes up when a button is clicked

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -7,9 +9,10 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     connect(ui->horizontalSlider, SIGNAL(valueChanged(int)), ui->progressBar, SLOT(setValue(int)) );
-    // if we disconnect it
+    // if we want to disconnect it
     //disconnect(ui->horizontalSlider, SIGNAL(valueChanged(int)), ui->progressBar, SLOT(setValue(int)) );
 }
+
 
 MainWindow::~MainWindow()
 {
@@ -20,4 +23,11 @@ MainWindow::~MainWindow()
 void MainWindow::on_pushButton_clicked()
 {
     ui->label->setText("Some text");
+}
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    // test with critical, information, question
+    QMessageBox::critical(this, tr("The title"), tr("Message"));
 }
