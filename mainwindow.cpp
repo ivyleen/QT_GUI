9bf330667182af23d2f55b77fe4bfeea98ae18a6
Ivy Ivanova <ivaivanova091@gmail.com>
2020-06-25 12:50:34 +0100

    [bug fix] message: No matching signal for on_pushButton_2_clicked()

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,13 +3,14 @@
 #include <QMessageBox>
 // for opening a new window with model approach (everything on the stack)
 //#include "seconddialog.h"
-
+#include "login.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
     connect(ui->horizontalSlider, SIGNAL(valueChanged(int)), ui->progressBar, SLOT(setValue(int)) );
     // if we want to disconnect it
     //disconnect(ui->horizontalSlider, SIGNAL(valueChanged(int)), ui->progressBar, SLOT(setValue(int)) );
@@ -27,23 +28,16 @@ void MainWindow::on_pushButton_clicked()
     ui->label->setText("<html><b>First line</b><br><u>Second</u> line</html>");
 }
 
-
-void MainWindow::on_pushButton_2_clicked()
-{
-    // test with critical, information, question, warning
-    QMessageBox::critical(this, tr("The title"), tr("Message"));
-}
-
-
 void MainWindow::on_newWindow_clicked()
 {
     // after the button is clicked open the new window
-    // modal approach
+    // modal approach - you will have a window open which will prevent working with the main window
     //SecondDialog secDialogObj;
     //secDialogObj.setModal(true);
     //secDialogObj.exec();
 
-    //modeless approach
+    //modeless approach - you will have n numbers of windows depends on how many times you click on
+    // the button: the main window is reachable
     secDialogObj = new SecondDialog(this);
     secDialogObj->show();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -19,8 +19,6 @@ public:
 private slots:
     void on_pushButton_clicked();
 
-    void on_pushButton_2_clicked();
-
     void on_newWindow_clicked();
 
 private:
